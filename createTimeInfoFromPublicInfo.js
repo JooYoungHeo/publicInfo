3a01adde4aa157b4126a19898a9f72096c4da5bf
JooYoungHeo <jyhur900226@naver.com>
2017-10-12 20:11:40 +0900

    [작업] timetable xml 정보 파싱 1단계

--- createTimeInfoFromPublicInfo.js
+++ createTimeInfoFromPublicInfo.js
@@ -6,20 +6,51 @@ let xml2js = require('xml2js').parseString;
 let {Station} = require(path.join(process.cwd(), 'models'));
 
 let publicKey = config.public.key;
+let url = 'http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getSubwaySttnAcctoSchdulList';
+let line = 'SES08호선';
+let upDownCodeList = ['D', 'U'];
+let dailyCodeList = ['01', '02', '03'];
 
-findStation().then(result => {
-  result.forEach(item => {
-    
-  });
+findStation(line).then(result => {
+  // result.forEach(item => {
+    let stationId = result[0]._id;
+    for (let i in dailyCodeList) {
+      let dailyCode = dailyCodeList[i];
+      for (let j in upDownCodeList) {
+        let upDownCode = upDownCodeList[j];
+        requestScheduleUrl(stationId, dailyCode, upDownCode);
+      }
+    }
+  // });
 });
 
-function findStation() {
+function findStation(line) {
   return new Promise((resolve, reject) => {
     Station.find({
-      line: 'SES08호선'
+      line: line
     }).exec((err, item) => {
       if(err) reject(err);
       else resolve(item);
+    });
+  });
+}
+
+function requestScheduleUrl(stationId, dailyCode, upDownCode) {
+  let pageNo = 1;
+  let requestUrl = `${url}?ServiceKey=${publicKey}&subwayStationId=${stationId}&dailyTypeCode=${dailyCode}&upDownTypeCode=${upDownCode}&pageNo=${pageNo}`;
+
+  request(requestUrl, (err, res, body) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    xml2js(body, (err, result) => {
+      let items = result.response.body[0].items[0].item;
+      if (items === undefined) return;
+
+      for (let i = 0 ; i < items.length ; i++) {
+        console.log(`${dailyCode} - ${upDownCode} - ${items[i].arrTime}`);
+      }
     });
   });
 }
