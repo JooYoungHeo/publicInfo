ed7153864f32e5a7bb5b8bd30ef23812fe5b5342
JooYoungHeo <jyhur900226@naver.com>
2017-10-13 09:06:20 +0900

    [작업] async 모듈 적용 (target: one item)

--- createTimeInfoFromPublicInfo.js
+++ createTimeInfoFromPublicInfo.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let path = require('path');
 let request = require('request');
+let async = require('async');
 let config = require('./config');
 let xml2js = require('xml2js').parseString;
 let {Station} = require(path.join(process.cwd(), 'models'));
@@ -11,19 +12,21 @@ let line = 'SES08호선';
 let upDownCodeList = ['D', 'U'];
 let dailyCodeList = ['01', '02', '03'];
 
-findStation(line).then(result => {
-  result.forEach(item => {
-    let stationId = item._id;
-    for (let i in dailyCodeList) {
-      let dailyCode = dailyCodeList[i];
-      for (let j in upDownCodeList) {
-        let upDownCode = upDownCodeList[j];
+requestScheduleUrl('SES2814', '01', 'D');
 
-        requestScheduleUrl(stationId, dailyCode, upDownCode);
-      }
-    }
-  });
-});
+// findStation(line).then(result => {
+//   result.forEach(item => {
+//     let stationId = item._id;
+//     for (let i in dailyCodeList) {
+//       let dailyCode = dailyCodeList[i];
+//       for (let j in upDownCodeList) {
+//         let upDownCode = upDownCodeList[j];
+//
+//         requestScheduleUrl(stationId, dailyCode, upDownCode);
+//       }
+//     }
+//   });
+// });
 
 function findStation(line) {
   return new Promise((resolve, reject) => {
@@ -38,20 +41,37 @@ function findStation(line) {
 
 function requestScheduleUrl(stationId, dailyCode, upDownCode) {
   let pageNo = 1;
-  let requestUrl = `${url}?ServiceKey=${publicKey}&subwayStationId=${stationId}&dailyTypeCode=${dailyCode}&upDownTypeCode=${upDownCode}&pageNo=${pageNo}`;
+  let flag = true;
 
-  request(requestUrl, (err, res, body) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    xml2js(body, (err, result) => {
-      let items = result.response.body[0].items[0].item;
-      if (items === undefined) return;
+  async.whilst(function() {
+    return flag;
+  }, function(callback) {
+    let requestUrl = `${url}?ServiceKey=${publicKey}&subwayStationId=${stationId}&dailyTypeCode=${dailyCode}&upDownTypeCode=${upDownCode}&pageNo=${pageNo}`;
 
-      for (let i = 0 ; i < items.length ; i++) {
-        console.log(`${stationId} - ${dailyCode} - ${upDownCode} - ${items[i].arrTime}`);
+    request(requestUrl, (err, res, body) => {
+      if (err) {;
+        flag = false;
+        callback();
+        return;
       }
+
+      xml2js(body, (err, result) => {
+        let items = result.response.body[0].items[0].item;
+        if (items === undefined) {
+          flag = false;
+          callback();
+          return;
+        }
+
+        for (let i = 0 ; i < items.length ; i++) {
+          console.log(`${stationId} - ${dailyCode} - ${upDownCode} - ${items[i].arrTime}`);
+        }
+
+        pageNo++;
+        callback();
+      });
     });
+  }, function(err) {
+    console.log('done');
   });
 }
